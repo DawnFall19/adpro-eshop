320a1032c34876bee886a251d7ebd0215f5dc040
DawnFall19 <ignasius.michael09@gmail.com>
2025-02-14 00:29:12 +0700

    Implementing edit function

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -22,10 +22,20 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
+    public Product edit(String id, String newName, int newQuantity) {
+        return productRepository.edit(id, newName, newQuantity);
+    }
+
+    @Override
     public List<Product> findAll() {
         Iterator<Product> productIterator = productRepository.findAll();
         List<Product> allProduct = new ArrayList<>();
         productIterator.forEachRemaining(allProduct::add);
         return allProduct;
+    }
+
+    @Override
+    public Product findByID(String id) {
+        return productRepository.findByID(id);
     }
 }
