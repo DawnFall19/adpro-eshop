8fb2b3e78a492b3bd8d9c9d477da8962e662feb0
DawnFall19 <ignasius.michael09@gmail.com>
2025-02-14 16:48:23 +0700

    Adding unit-test for edit and delete

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -16,15 +16,36 @@ class ProductTest {
     @Test
     void testGetProductId() {
         assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", this.product.getProductId());
+        assertNotEquals("a0f9de46-90b1-437d-a0bf-d0821dde9096", this.product.getProductId());
     }
 
     @Test
     void testGetProductName() {
         assertEquals("Sampo Cap Bambang", this.product.getProductName());
+        assertNotEquals("Sampo Cap Usep", this.product.getProductName());
     }
 
     @Test
     void testGetProductQuantity() {
         assertEquals(100, this.product.getProductQuantity());
+        assertNotEquals(50, this.product.getProductQuantity());
+    }
+
+    @Test
+    void testGetAfterSetId() {
+        this.product.setProductId("a0f9de46-90b1-437d-a0bf-d0821dde9096");
+        assertEquals("a0f9de46-90b1-437d-a0bf-d0821dde9096", this.product.getProductId());
+    }
+
+    @Test
+    void testGetAfterSetProductName() {
+        this.product.setProductName("Sampo Cap Usep");
+        assertEquals("Sampo Cap Usep", this.product.getProductName());
+    }
+
+    @Test
+    void testGetAfterSetProductQuantity() {
+        this.product.setProductQuantity(50);
+        assertEquals(50, this.product.getProductQuantity());
     }
 }
