b13dfb091a107cc479746c904a361205911a5e3b
DawnFall19 <ignasius.michael09@gmail.com>
2025-02-28 19:27:02 +0700

    Implementing SRP

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -0,0 +1,62 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.service.CarService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/car")
+public class CarController {
+
+    private final CarService carService;
+    private final String modelAttributeName = "car";
+    private final String backToProductList = "redirect:/car/listCar";
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping("/createCar")
+    public String createCarPage(Model model) {
+        Car car = new Car();
+        model.addAttribute(modelAttributeName, car);
+        return "CreateCar";
+    }
+
+    @PostMapping("/createCar")
+    public String createCarPost(@ModelAttribute Car car) {
+        carService.create(car);
+        return backToProductList;
+    }
+
+    @GetMapping("/listCar")
+    public String carListPage(Model model) {
+        List<Car> allCars = carService.findAll();
+        model.addAttribute("cars", allCars);
+        return "CarList";
+    }
+
+    @GetMapping("/editCar/{carId}")
+    public String editCarPage(@PathVariable String carId, Model model) {
+        Car car = carService.findById(carId);
+        model.addAttribute(modelAttributeName, car);
+        return "EditCar";
+    }
+
+    @PostMapping("/editCar")
+    public String editCarPost(@ModelAttribute Car car) {
+        System.out.println(car.getCarId());
+        carService.update(car.getCarId(), car);
+        return backToProductList;
+    }
+
+    @PostMapping("/deleteCar")
+    public String deleteCar(@RequestParam("carId") String carId) {
+        carService.deleteCarById(carId);
+        return backToProductList;
+    }
+}
